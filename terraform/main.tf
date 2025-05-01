6f0094ecd0d42ee54b09846ee6c146d120d9aead
tuananhlai <42168740+tuananhlai@users.noreply.github.com>
2025-05-01 10:16:58 +0900

    feat/terraform: add vpc

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,21 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "default" {
-  cidr_block = "10.0.0.0/16"
+locals {
+  default_vpc_cidr = "10.0.0.0/16"
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.21.0"
+
+  name            = "BrevityVPC"
+  cidr            = local.default_vpc_cidr
+  azs             = slice(data.aws_availability_zones.available.names, 0, 2)
+  public_subnets  = ["10.0.0.0/20", "10.0.16.0/20"]
+  private_subnets = ["10.0.32.0/20", "10.0.48.0/20"]
 }
