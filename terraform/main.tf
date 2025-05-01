a78281d31436312444030d0bb936101621aba797
tuananhlai <42168740+tuananhlai@users.noreply.github.com>
2025-05-01 21:11:13 +0900

    feat/terraform: add database

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,3 +20,30 @@ module "vpc" {
   public_subnets  = ["10.0.0.0/20", "10.0.16.0/20"]
   private_subnets = ["10.0.32.0/20", "10.0.48.0/20"]
 }
+
+resource "random_password" "db_password" {
+  length  = 16
+  special = true
+  upper   = true
+  numeric = true
+}
+
+resource "aws_db_instance" "primary" {
+  identifier_prefix   = "brevity-db"
+  engine              = "postgres"
+  instance_class      = "db.t4g.micro"
+  allocated_storage   = 20
+  skip_final_snapshot = true
+  db_name             = "brevity"
+  username            = "postgres"
+  password            = random_password.db_password.result
+  apply_immediately   = true
+}
+
+output "primary" {
+  value = {
+    password = nonsensitive(random_password.db_password.result),
+    username = aws_db_instance.primary.username
+    address  = aws_db_instance.primary.address
+  }
+}
