4a3f92dffb3e5985421b88c27c0078320c7ab6ed
Lại Tuấn Anh <42168740+tuananhlai@users.noreply.github.com>
2025-05-05 21:45:58 +0900

    feat/ci: build docker image and deploy ecr on merging to main (#17)
    * feat/ecs: initialize
    
    * minor changes
    
    * minor changes
    
    * minor changes
    
    * minor changes
    
    * minor changes
    
    * minor changes
    
    * minor changes

---
+++ terraform/github_actions.tf
@@ -0,0 +1,89 @@
+// https://aws.amazon.com/blogs/security/use-iam-roles-to-connect-github-actions-to-actions-in-aws/
+// https://docs.github.com/en/actions/security-for-github-actions/security-hardening-your-deployments/configuring-openid-connect-in-amazon-web-services#configuring-the-role-and-trust-policy
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+}
+
+// IAM Role for Github Actions workflow to assume. It allows deployment of ECR images and ECS services.
+resource "aws_iam_role" "github_actions" {
+  name_prefix = "brevity-github-actions-"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          // Note that the sub claim must use `StringLike` instead of `StringEquals`.
+          // See the docs.github.com link at the beginning of this file.
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:tuananhlai/brevity-go:*",
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "github_actions" {
+  name_prefix = "ecr-ecs-deploy-"
+  role        = aws_iam_role.github_actions.name
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid    = "ECRAccess",
+        Effect = "Allow",
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload"
+        ],
+        Resource = "*"
+      },
+      {
+        Sid    = "ECSAccess",
+        Effect = "Allow",
+        Action = [
+          "ecs:DescribeServices",
+          "ecs:DescribeTaskDefinition",
+          "ecs:RegisterTaskDefinition",
+          "ecs:UpdateService"
+        ],
+        Resource = "*"
+      },
+      {
+        Sid : "Logs",
+        Effect : "Allow",
+        Action : [
+          "logs:DescribeLogGroups",
+          "logs:DescribeLogStreams",
+          "logs:GetLogEvents"
+        ],
+        Resource : "*"
+      }
+    ]
+  })
+}
+
+output "github_actions" {
+  value = {
+    arn = aws_iam_role.github_actions.arn
+  }
+}
