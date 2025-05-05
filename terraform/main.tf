8a07777743a668b5fbab22dd9c3fbc3f3cb69db3
Lai Tuan Anh <42168740+tuananhlai@users.noreply.github.com>
2025-05-05 21:13:50 +0900

    minor changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -253,6 +253,85 @@ resource "aws_lb_listener" "ecs" {
   }
 }
 
+// == Github Actions ==
+// https://aws.amazon.com/blogs/security/use-iam-roles-to-connect-github-actions-to-actions-in-aws/
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+}
+
+resource "aws_iam_role" "github_actions" {
+  name_prefix = "brevity-github-actions-"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+            "token.actions.githubusercontent.com:sub" = "repo: tuananhlai/brevity-go:ref:refs/heads/main",
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "github_actions" {
+  name_prefix = "ecr-ecs-deploy-"
+  role        = aws_iam_role.github_actions.name
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid    = "ECRAccess",
+        Effect = "Allow",
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload"
+        ],
+        Resource = "*"
+      },
+      {
+        Sid    = "ECSAccess",
+        Effect = "Allow",
+        Action = [
+          "ecs:DescribeServices",
+          "ecs:DescribeTaskDefinition",
+          "ecs:RegisterTaskDefinition",
+          "ecs:UpdateService"
+        ],
+        Resource = "*"
+      },
+      {
+        Sid : "Logs",
+        Effect : "Allow",
+        Action : [
+          "logs:DescribeLogGroups",
+          "logs:DescribeLogStreams",
+          "logs:GetLogEvents"
+        ],
+        Resource : "*"
+      }
+    ]
+  })
+}
+
 output "primary_db" {
   value = {
     password = nonsensitive(random_password.db_password.result),
@@ -266,5 +345,11 @@ output "ecr" {
   value = {
     name = aws_ecr_repository.default.name
     url  = aws_ecr_repository.default.repository_url
+  }
+}
+
+output "github_actions" {
+  value = {
+    arn = aws_iam_role.github_actions.arn
   }
 }
