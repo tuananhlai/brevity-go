fc83e22a11074884c59ecf09fde4c9be34b02099
Lai Tuan Anh <42168740+tuananhlai@users.noreply.github.com>
2026-02-14 11:41:06 +0000

    more changes

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-brew install golang-migrate mockery opentofu gh
+brew install golang-migrate mockery opentofu gh httpie
 # golangci-lint **should not** be installed via brew since it can cause brew to install a different version
 # of Go
 curl -sSfL https://golangci-lint.run/install.sh | sh -s -- -b $(go env GOPATH)/bin v2.9.0
