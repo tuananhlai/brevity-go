6a95d4dcf837fb72e8216eb6d40231c541c32a12
tuananhlai <42168740+tuananhlai@users.noreply.github.com>
2026-02-16 13:37:29 +0000

    feat/devcontainer: use docker compose

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-brew install golang-migrate mockery opentofu gh httpie
+brew install golang-migrate mockery opentofu gh httpie pgcli
 # golangci-lint **should not** be installed via brew since it can cause brew to install a different version
 # of Go
 curl -sSfL https://golangci-lint.run/install.sh | sh -s -- -b $(go env GOPATH)/bin v2.9.0
