03ca99a5a6e340770ad3f1285d9e9ecd66e2c1fc
Lai Tuan Anh <42168740+tuananhlai@users.noreply.github.com>
2026-02-28 13:59:55 +0900

    chore/devcontainer: update

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-brew install golang-migrate mockery opentofu gh httpie pgcli codex
-# golangci-lint **should not** be installed via brew since it can cause brew to install a different version
-# of Go
-curl -sSfL https://golangci-lint.run/install.sh | sh -s -- -b $(go env GOPATH)/bin v2.9.0
+# Set the ZSH theme to "robbyrussell".
+sed -i '/^ZSH_THEME/c\ZSH_THEME="robbyrussell"' ~/.zshrc
